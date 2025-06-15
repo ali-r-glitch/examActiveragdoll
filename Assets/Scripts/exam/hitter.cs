c71f607611c7fd7bdf07f92e0a52f060edfb3cb6
ali-r-glitch <aliraffee2@gmail.com>
2025-06-16 01:15:30 +0200

    added hitter script

---
+++ Assets/Scripts/exam/hitter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitter : MonoBehaviour
+{
+ private Rigidbody rb;
+
+ private void Start()
+ {
+  rb = GetComponent<Rigidbody>();
+ }
+
+ private void OnCollisionEnter(Collision other)
+ {
+  Vector3 relativeVelocity = other.relativeVelocity;
+
+
+  float impactForce = rb.mass * relativeVelocity.magnitude / Time.fixedDeltaTime;
+
+  Debug.Log("Impact force: " + impactForce);
+ }
+}
